9b99c4b0bbc93ea8c3b9fcb6c701339b11f62f71
Rahu Singh <ashendrasingh100@gmail.com>
2026-01-25 23:31:36 +0530

    refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+import { AboutUs } from './components/about-us/about-us';
+import { ContactUs } from './components/contact-us/contact-us';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  { path: 'contact-us', component: ContactUs },
+  { path: 'about-us', component: AboutUs },
+];
